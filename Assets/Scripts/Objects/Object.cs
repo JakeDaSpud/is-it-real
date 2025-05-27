ff1156774070a13f76748575e9a1c537a7152449
Jake O'Reilly <78006039+JakeDaSpud@users.noreply.github.com>
2025-05-28 01:20:08 +0200

    :sparkles: finished (for now?) the Object.cs, made a couch prefab object

---
+++ Assets/Scripts/Objects/Object.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Object : MonoBehaviour
+{
+	[Header("Basic Info")]
+	[SerializeField] private string objectName = "Unnamed_Hauntable_Object";
+	[SerializeField] private bool isActive = true;
+
+	[Header("Visuals")]
+	[SerializeField] private bool hasSprite = true;
+	[SerializeField] private bool showSprite = true;
+	[SerializeField] private SpriteRenderer spriteRenderer;
+	[SerializeField] private Sprite[] sprites;
+
+	[Header("Collision")]
+	[Tooltip("The Collider of the Object, where the Player can't walk.")]
+	[SerializeField] private Collider2D hitBox;
+
+	[Tooltip("The Collider that the Player can interact with.")]
+	[SerializeField] private Collider2D interactBox;
+
+	[Header("Haunting")]
+	public bool canBeHaunted = false;
+	public GameObject hauntingAnomaly; // null unless haunted!
+
+	[Header("Hovering")]
+	private bool isHovered = false;
+	private bool isSelected = false;
+
+	private enum SpriteUpdateMode { NONE, PROGRESS, RANDOM_CHANGE }
+	[Tooltip("NONE: Sprite will not update. PROGRESS: Sprite will update at the start of each day to the next Sprite. RANDOM_CHANGE: Randomly swap to a DIFFERENT Sprite.")]
+	[SerializeField] private SpriteUpdateMode updateMode = SpriteUpdateMode.NONE;
+	private int currentSpriteIndex = 0;
+
+
+	void Awake()
+	{
+		if (!isActive)
+		{
+			this.gameObject.SetActive(false);
+			return;
+		}
+
+		if (hasSprite && showSprite)
+		{
+			if (updateMode != SpriteUpdateMode.NONE)
+			{
+				if (sprites == null)
+				{
+					throw new System.Exception($"updatingSprite will try to update [{objectName}], but a Sprite[] Array is not assigned.");
+				}
+
+				if (sprites.Length < 2)
+				{
+					throw new System.Exception($"updatingSprite will try to update [{objectName}], but there's no Sprites to switch to.");
+				}
+			}
+
+			spriteRenderer.sprite = sprites[currentSpriteIndex];
+		}
+
+		else
+		{
+			spriteRenderer.gameObject.SetActive(false);
+		}
+	}
+
+	public void UpdateObject()
+	{
+		// Default sprite set
+		Sprite[] currentSpriteArray = sprites;
+
+		// Check which update option is being used, update accordingly
+		if (this.updateMode == SpriteUpdateMode.NONE)
+		{
+			return;
+		}
+
+		if (this.updateMode == SpriteUpdateMode.PROGRESS)
+		{
+			if (currentSpriteIndex >= currentSpriteArray.Length)
+			{
+				currentSpriteIndex = currentSpriteArray.Length - 1;
+			}
+
+			else
+			{
+				currentSpriteIndex++;
+			}
+
+			spriteRenderer.sprite = currentSpriteArray[currentSpriteIndex];
+
+			Debug.Log($"[{objectName}] PROGRESSED Sprite.");
+			return;
+		}
+
+		if (this.updateMode == SpriteUpdateMode.RANDOM_CHANGE)
+		{
+			currentSpriteIndex = Random.Range(0, currentSpriteArray.Length);
+
+			spriteRenderer.sprite = currentSpriteArray[currentSpriteIndex];
+
+			Debug.Log($"[{objectName}] changed Sprite RANDOMLY.");
+			return;
+		}
+	}
+
+	public void Interact(Collider2D other)
+	{
+		if (other.tag == "Player" && isActive)
+		{
+			Debug.Log($"Player interacted with [{objectName}].");
+		}
+	}
+
+	// Called by Hover System (doesn't exist yet)
+	public void SetHover(bool hover)
+	{
+		isHovered = hover;
+		// Change material / shader here
+		// Glow effect?
+    }
+
+    public void Select()
+    {
+        if (!isHovered) return;
+
+        isSelected = true;
+        Debug.Log($"[{objectName}] selected by player.");
+
+		if (hauntingAnomaly != null)
+		{
+			Debug.LogWarning($"[{objectName}] is haunted by [{hauntingAnomaly.name}]!");
+        }
+
+		Debug.Log($"This is where [{objectName}] tries to get added to the end-day bestiary to check if it's haunted!");
+    }
+}
